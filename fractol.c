2d6a32653eb695862dd37006a90157c2cb7d83c2
Saikiran Prodaturu <sprodaturu@gmail.com>
2024-07-01 17:43:13 +0200

    updated comments

--- fractol.c
+++ fractol.c
@@ -6,11 +6,7 @@
 /*   By: sprodatu <sprodatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 08:10:52 by sprodatu          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/05/03 02:35:01 by sprodatu         ###   ########.fr       */
-=======
-/*   Updated: 2024/05/03 02:22:12 by sprodatu         ###   ########.fr       */
->>>>>>> master
+/*   Updated: 2024/07/01 17:39:02 by sprodatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,22 +66,15 @@ void	zooming(double x_diff, double y_diff, void *param)
  * 		sets the xmin, xmax, ymin, ymax, cre, cim values for the Julia fractal.
  * If arguments are un-valid,
  * 		it prints an error message and exits the program.
-<<<<<<< HEAD
-=======
  *
  * @param f A pointer to the t_fractal structure.
  * @param c An array of strings containing the command-line arguments.
  * @param mlx The MLX library instance.
  * @param argc The number of command-line arguments.
->>>>>>> master
  * 
  * @return void
  * 
  * @mlx_loop_hook: Register the Mandelbrot or Julia hook.
-<<<<<<< HEAD
- */
-
-=======
  * @write: Print an error message.
  * @exit: Exit the program.
  */
@@ -102,7 +91,6 @@ void	zooming(double x_diff, double y_diff, void *param)
  * 	- print error message and exit
 */
 
->>>>>>> master
 void	fract_modalu(t_fractal *f, char **argv, void *mlx, int argc)
 {
 	if (*argv[1] != 'm' && *argv[1] != 'j' && *argv[1] != 'b')
@@ -144,15 +132,11 @@ void	fract_modalu(t_fractal *f, char **argv, void *mlx, int argc)
  * 	If the image is not displayed successfully (i.e. return -1)
  * 		closes the window, errno is set, returns EXIT_FAILURE.
  *
-<<<<<<< HEAD
- * Return 1 if successful, EXIT_FAILURE otherwise.
-=======
  * Return 1 if successful
  * 
  * @param mlx The mlx_t structure to initialize.
  * @param f The t_fractal structure containing the image.
  * @return 1 if successful, EXIT_FAILURE otherwise.
->>>>>>> master
  */
 
 int	img_modhalu(void *mlx, t_fractal *f)
@@ -215,11 +199,7 @@ int	main(int argc, char **argv)
 }
 
 /**
-<<<<<<< HEAD
- * @logic: Detailed explanation:
-=======
  * @logic:
->>>>>>> master
  * 
  * if more than one argument is provided
  * 		Initialize the MLX library
